465c98ebf854991751df016a6c3d2d98c6cba32e
Nate Reed <Nate@Belnine.com>
2019-04-17 16:21:22 -0700

    add jwt tokens and Auth to API Backend

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -36,9 +37,23 @@ password: {
             throw new Error('Password can not contain "password"')
         }
     }
-}
+},
+tokens: [{
+    token:{
+        type: String,
+        required: true
+    }
+}]
 })
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'TCG-Software-1')
 
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
 userSchema.statics.findByCredentials = async (username, password) => {
     const user = await User.findOne({ username })
     if(!username) {
--- src/routers/users.js
+++ src/routers/users.js
@@ -11,6 +11,7 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try{
         await user.save()
+        const token = await user.generateAuthToken()
         res.status(201).send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -20,7 +21,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try{
        const user = await User.findByCredentials(req.body.username, req.body.password)
-        res.status(200).send(user)
+       const token = await user.generateAuthToken() 
+       res.send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
